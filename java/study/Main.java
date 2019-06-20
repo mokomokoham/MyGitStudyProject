1fd9a6f16df5dc0e68160418bd7596d81f9af4de
mokomokoham <39557929+mokomokoham@users.noreply.github.com>
2019-06-20 09:50:32 +0900

    upload first folder
    i uploaded !!!

---
+++ java/study/Main.java
@@ -0,0 +1,17 @@
+public class Main{
+  public static void main(String[] args){
+
+    System.out.println(cal(100,200));
+
+
+  }
+
+
+  protected static int cal(int a, int b){
+
+    int cal = a + b;
+
+    return cal;
+  }
+
+}
